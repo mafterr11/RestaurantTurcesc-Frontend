6e4485cc39bb899899dc71d190d32c518bfe3298
mafterr11 <mafterr11@gmail.com>
2023-11-20 20:47:24 +0200

    Auth Complete

--- app/auth/login/page.jsx
+++ app/auth/login/page.jsx
@@ -1,64 +1,12 @@
-"use client";
-import { login } from "@/app/services/auth";
-import { useState } from "react";
+"use server"
 
-const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+import { login } from "../../services/auth";
+import Login from "../../components/Login";
 
-  // const login = () => {
-  //   fetch("http://localhost:8080/api/v1/auth/login", {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({ email, password }),
-  //   })
-  //     .then((data) => data.json())
-  //     .then((data) => console.log(data));
-  // };
-
+const LoginPage = () => {
   return (
-    <div className="bg-black/40 absolute top-0 bottom-0 left-0 right-0">
-    <div className='max-w-[500px] mx-auto my-[10rem] overflow-auto min-h-[300px] border-2 border-black border-opacity-86 shadow-standard p-5 rounded-15  bg-black bg-opacity-90 text-white'>
-      <h1>Login</h1>
-      <form>
-        <div className='my-5'>
-          <label className='block font-bold text-[20px]'>Email</label>
-          <input
-            className='w-full h-10 my-2 p-2 text-lg border-[1px] border-black text-black'
-            type='text'
-            placeholder='Email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            />
-        </div>
-
-        <div className='my-5'>
-          <label className='block font-bold text-[20px]'>Password</label>
-          <input
-            className='w-full h-10 my-2 p-2 text-lg border-[1px] border-black text-black'
-            type='password'
-            placeholder='Password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            />
-        </div>
-        <input
-          type='submit'
-          value='Login'
-          className='btn btn-block'
-          onClick={async(e) => {
-            e.preventDefault();
-            e.stopPropagation();
-            const response = await login(email, password);
-            console.log(response);
-          }}
-          />
-      </form>
-    </div>
-    </div>
+    <Login login={login} />
   );
 };
 
-export default Login;
+export default LoginPage;
--- app/auth/register/page.jsx
+++ app/auth/register/page.jsx
@@ -1,78 +1,11 @@
-"use client";
-import { register } from "@/app/services/auth";
-import { useState } from "react";
+"use client"
+import { register } from "../../services/auth";
+import Register from "../../components/Register"; 
  
- 
-const Register = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
+const RegisterPage = () => {
   return (
-    <div className="bg-black/40 absolute top-0 bottom-0 left-0 right-0">
-    <div className='max-w-[500px] mx-auto sm:my-[10rem] my-[8rem] overflow-auto min-h-[300px] border-2 border-black border-opacity-86 shadow-standard p-5 rounded-15  bg-black bg-opacity-90 text-white'>
-      <h1>Register new account</h1>
-      <form>
-        <div className='my-5'>
-          <label className='block font-bold text-[20px]'>First Name</label>
-          <input
-            className='w-full h-10 my-2 p-2 text-lg border-[1px] border-black text-black'
-            type='text'
-            placeholder='First Name'
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-        </div>
-
-        <div className='my-5'>
-          <label className='block font-bold text-[20px]'>Last Name</label>
-          <input
-            className='w-full h-10 my-2 p-2 text-lg border-[1px] border-black text-black'
-            type='text'
-            placeholder='Last Name'
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-          />
-        </div>
-
-        <div className='my-5'>
-          <label className='block font-bold text-[20px]'>Email</label>
-          <input
-            className='w-full h-10 my-2 p-2 text-lg border-[1px] border-black text-black'
-            type='text'
-            placeholder='Email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-
-        <div className='my-5'>
-          <label className='block font-bold text-[20px]'>Password</label>
-          <input
-            className='w-full h-10 my-2 p-2 text-lg border-[1px] border-black text-black'
-            type='password'
-            placeholder='Password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-
-        <input
-          type='submit'
-          value='Register'
-          className='btn btn-block'
-          onClick={async(e) => {
-            e.preventDefault();
-            e.stopPropagation();
-            const response = await register(firstName, lastName, email, password);
-            console.log(response);
-          }}
-        />
-      </form>
-    </div>
-    </div>
+    <Register register={register}/>
   );
 };
 
-export default Register;
+export default RegisterPage;
