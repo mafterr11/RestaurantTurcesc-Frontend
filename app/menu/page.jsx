de4f035a2414330b710476853a4feee2014d99b4
mafterr11 <mafterr11@gmail.com>
2023-11-20 20:15:47 +0200

    Commit

--- app/menu/page.jsx
+++ app/menu/page.jsx
@@ -2,33 +2,14 @@ import Product from "../components/Product";
 import { isLoggedIn, getToken } from "../services/auth";
 
 const fetchProducts = async () => {
-  const res = await fetch("http://localhost:8080/api/v1/products/findAll", { cache: 'no-store' });
-  // console.log(res);
+  const res = await fetch("http://localhost:8080/api/v1/products/findAll", { cache: 'no-store' }, {headers: {Authorization: getToken()}});
   return await res.json();
 };
 
 const Products = async () => {
-  // PRIMA METODA
   const products = await fetchProducts();
-
-  console.log(isLoggedIn());
-  console.log(getToken());
-
-  // A DOUA METODA
-  // const [products, setProducts] = useState([]);
-
-  // useEffect(() => {
-  //   const getProducts = async () => {
-  //     const productsFromServer = await fetchProducts();
-  //     setProducts(productsFromServer);
-  //   };
-  //   getProducts();
-  // }, []);
-
-  // const fetchProducts = async () => {
-  //   const res = await fetch("http://localhost:8080/api/v1/products");
-  //   return await res.json();
-  // };
+  // console.log(isLoggedIn());
+  // console.log(getToken());
   return (
     <div>
       {products.map((product) => (
---
+++ app/services/auth.jsx
@@ -0,0 +1,49 @@
+import { cookies } from "next/headers";
+
+export const login = async (email, password) => {
+  const res = await fetch("http://localhost:8080/api/v1/auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  });
+  const result = await res.json();
+  cookies().set({
+    name: "token",
+    value: result.token,
+  });
+  return result;
+};
+
+export const register = async (firstName, lastName, email, password) => {
+  const res = await fetch("http://localhost:8080/api/v1/auth/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ firstName, lastName, email, password }),
+  });
+  const result = await res.json();
+  // Cookies.set("token", result.token);
+  cookies().set({
+    name: "token",
+    value: result.token,
+  });
+  return result;
+};
+
+export const logout = () => {
+  // Cookies.remove("token");
+};
+
+export const isLoggedIn = () => {
+  const token = cookies().get("token");
+  console.log(token);
+  if (token) return true;
+  return false;
+};
+
+export const getToken = () => {
+  return cookies().get("token")?.value;
+};
