ac2662931a78a69d4116b1710463bf3c3bc276bf
mafterr11 <mafterr11@gmail.com>
2023-11-21 21:49:36 +0200

    Commit

--- app/menu/page.jsx
+++ app/menu/page.jsx
@@ -3,9 +3,9 @@ import CategoryList from "../components/CategoryList";
 
 const Products = async () => {
   return (
-    <div className='bg-black/40 absolute top-0 left-0 right-0 sm:py-[10rem] sm:px-40'>
-      <h1 className=' text-center text-5xl mb-24'>MENU DELIRIUM</h1>
+    <div className='lg:py-[10rem] lg:px-40 pt-[4.3rem] bg-black/40'>
       <div className='bg-black/60 pt-2'>
+      <h1 className=' text-center text-5xl mb-24 mt-16'>MENU DELIRIUM</h1>
         <CategoryList />
         <ProductList />
       </div>
