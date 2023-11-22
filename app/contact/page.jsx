a8a6864a66e1f7f247eff9901c318be8df8b6e5a
Alexandru Maftei <mafterr11@gmail.com>
2023-11-22 14:23:57 +0200

    Commit

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -12,8 +12,8 @@ export const metadata = {
 const page = () => {
   return (
     <div className="lg:py-[10rem] lg:px-40 pt-[4.3rem] bg-black/40">
-      <div className="bg-black/40 p-8 container mx-auto flex justify-center items-center flex-wrap-reverse sm:flex-nowrap h-screen">
-        <div className="lg:w-full lg:h-3/4 md:w-1/2 rounted-lg lg:mt-0 mt-12">
+      <div className="bg-black/40 lg:p-8 container mx-auto flex justify-center items-center flex-wrap-reverse sm:flex-nowrap h-screen">
+        <div className="w-full lg:w-full lg:h-3/4 md:w-1/2 rounted-lg mt-12 lg:mt-0 ">
         <iframe  src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d2848.0129493943437!2d26.05004825471762!3d44.45340569321442!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDTCsDI3JzEyLjMiTiAyNsKwMDMnMDYuNyJF!5e0!3m2!1sen!2sat!4v1700651270658!5m2!1sen!2sat" width="100%" height="100%" style={{border: "0"}} allowfullscreen="" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
 
