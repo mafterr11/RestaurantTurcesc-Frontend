3b5597c30e62418ef29acfe48cc1e6a7a142a3c3
mafterr11 <mafterr11@gmail.com>
2023-11-22 20:18:29 +0200

    Menu fix

--- app/components/Category.jsx
+++ app/components/Category.jsx
@@ -3,7 +3,7 @@ import ProductList from "./ProductList"
 const Category = ({ category }) => {
   return (
     <div>
-      <h1>{category.category.replace("_", " ")}</h1>
+      <h1 className="md:pt-12 text-3xl text-center underline underline-offset-[0.7rem]">{category.category.replace("_", " ")}</h1>
       <ProductList category={category.category} />
     </div>
   )
--- app/components/Product.jsx
+++ app/components/Product.jsx
@@ -1,9 +1,9 @@
 const Product = ({ product }) => {
   return (
-    <div className='my-12 px-24 pt-6'>
+    <div className='my-12 md:px-24 px-2 pt-6'>
       <div>
         <div className='flex justify-between items-baseline'>
-          <div className="whitespace-nowrap">
+          <div className="md:whitespace-nowrap md:text-xl">
             <h3>{product.name}</h3>
           </div>
           <div className='dots' />
