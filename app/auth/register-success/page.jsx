b46ba7f983128433e8397b1f645b1c8f126895fb
Alexandru Maftei <mafterr11@gmail.com>
2023-11-24 11:34:28 +0200

    Commit

--- app/auth/register-success/page.jsx
+++ app/auth/register-success/page.jsx
@@ -1,18 +1,12 @@
 import Link from "next/link";
 
-export const metadata = {
-    title: "Register - Delirium",
-    description: "Delirium register page",
-    keywords: "login, register, account, delirium login, delirium register, delirium account",
-};
-
 const RegisterSuccessPage = () => {
     return (
         <div className="overlay">
-            <div className="flex flex-col justify-center items-center gap-12 h-screen">
+            <div className="flex flex-col justify-center items-center gap-12 h-screen text-center">
                 <h1>CONGRATS, YOUR REGISTRATION HAS BEEN SUCCESSFUL</h1>
                 <h2 className="text-2xl">Go back to the <Link href="/" className="hover text-3xl"><span>{">"}Home{"<"}</span></Link> page</h2>
-                <h3 className="text-3xl">~~Enjoy~~</h3>
+                <h3 className="text-4xl">~~Enjoy~~</h3>
             </div>
         </div>
     )
